10de70dc6705cf3b95b2d604d1aa959d9ea7c711
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-04 16:59:31 +0100

    test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ restService.post('/map', function(req, resp) {
 
     rp(options)
         .then(function(res) {
-            console.log('test => ', req.body.result.contexts[0]);
+            console.log('test => ', req.body.result.contexts[5]);
             if (res[0].id == '') {
                 console.log('test if => ', res);
                 speech = "Désolé je n'ai pas compris votre recherche. Veuillez reformuler votre zone de recherche."
