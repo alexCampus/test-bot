ad97198c7a032e8f8dfce72f22f3e10865a7f88e
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-04 14:53:29 +0100

    test

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    // var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
-        speech: speech,
-        displayText: speech,
+        speech: 'Hello my friend',
+        displayText: 'Hello my friend',
         source: 'webhook-echo-sample'
     });
 });
