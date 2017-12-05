3b1e528ad02f4cdf577257d734acccbade9e6f14
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-05 17:20:31 +0100

    add axios

--- index.js
+++ index.js
@@ -65,16 +65,12 @@ restService.post('/map', function(req, resp) {
                 console.log('RESULT =>', resultats.length);
                 if (resultats.length == 0) {
                     console.log('NO RESULT');
-                    data = [
+                    data = 
                                 {
                                     "title" : "No Result",
                                     "image_url" : "https://i.vimeocdn.com/portrait/58832_300x300"
-                                },
-                                {
-                                    "title" : "No Result",
-                                    "image_url" : "https://i.vimeocdn.com/portrait/58832_300x300"
-                                }
-                            ];
+                                };
+
                     finalData.push(data);
                 } else {
                     resultats.prevObject.each(function (index) {
