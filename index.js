8c534d41c94838b1c978a458b248280bda6d232d
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-06 11:43:01 +0100

    test for axios requete

--- index.js
+++ index.js
@@ -41,13 +41,10 @@ restService.post('/map', function(req, resp) {
                 // speech = 'Ok je lance la recherche pour un/une ' + req.body.result.contexts[4].parameters.GoodType[0] + ' de ' + req.body.result.contexts[4].parameters.nbRoom + ' pieces minimum avec une surface de ' + req.body.result.contexts[4].parameters.minArea + ' m2 et pour un prix maximum de ' + req.body.result.contexts[4].parameters.maxPrice + ' dans le secteur de ' + req.body.result.contexts[4].parameters.location;
             }
             return speech;
-            // resp.json({
-            //     speech: speech,
-            //     displayText: speech,
-            //     source: 'webhook-echo-sample'
-            // });
+
         })
         .then(function(speech){
+            let type;
             parameters.localites = [
                 {
                     label: speech.label,
@@ -56,7 +53,12 @@ restService.post('/map', function(req, resp) {
                     type: parseInt(speech.type)
                 }
             ];
-            parameters.TYPE = 1;
+            if (req.body.result.contexts[0].parameters.GoodType === 'maison') {
+                type = 2;
+            } else if (req.body.result.contexts[0].parameters.GoodType === 'appartement'){
+                type = 1;
+            }
+            parameters.TYPE = type;
             parameters.NB_PIECES = req.body.result.contexts[0].parameters.nbRoom;
             parameters.SURFACE_MIN = req.body.result.contexts[0].parameters.minArea;
             parameters.PRIX_MAX = req.body.result.contexts[0].parameters.maxPrice;
@@ -129,67 +131,6 @@ restService.post('/map', function(req, resp) {
         .catch(function (error) {
             console.log(error);
         })
-    // var options = {
-    //     uri: configuration.fnaimUrlLocalization,
-    //     qs: {
-    //         term: speech
-    //     },
-    //     json: true
-    // };
-    //
-    // rp(options)
-    //     .then(function (res) {
-    //         console.log('test => ', req.body.result.contexts[4]);
-    //         if (res[0].id == '') {
-    //             console.log('test if => ', res);
-    //             speech = "Désolé je n'ai pas compris votre recherche. Veuillez reformuler votre zone de recherche."
-    //         } else {
-    //             console.log('test else => ', res);
-    //             // speech = 'Ok je lance la recherche pour un/une ' + req.body.result.contexts[4].parameters.GoodType[0] + ' de ' + req.body.result.contexts[4].parameters.nbRoom + ' pieces minimum avec une surface de ' + req.body.result.contexts[4].parameters.minArea + ' m2 et pour un prix maximum de ' + req.body.result.contexts[4].parameters.maxPrice + ' dans le secteur de ' + req.body.result.contexts[4].parameters.location;
-    //         }
-    //         resp.json({
-    //             speech: speech,
-    //             displayText: speech,
-    //             source: 'webhook-echo-sample'
-    //         });
-            // parameters.localites = res[0];
-            // parameters.TYPE = req.body.result.contexts[0].parameters.GoodType[0];
-            // parameters.NB_PIECES = req.body.result.contexts[0].parameters.nbRoom;
-            // parameters.SURFACE_MIN = req.body.result.contexts[0].parameters.minArea;
-            // parameters.PRIX_MAX = req.body.result.contexts[0].parameters.maxPrice;
-            // var choiceWebservice = rp({
-            //     url: url,
-            //     qs: parameters
-            // });
-            // choiceWebservice.then(function (result) {
-            //     // console.log('response =>', result);
-            //     resp.json({
-            //         speech: speech,
-            //         displayText: speech,
-            //         data: {
-            //             facebook: {
-            //                 attachment: {
-            //                     type: "template",
-            //                     payload: {
-            //                         template_type: "generic",
-            //                         elements: [
-            //                             {
-            //                                 "title": "Hello I am the card",
-            //                                 "image_url": "https://i.vimeocdn.com/portrait/58832_300x300"
-            //                             }
-            //                         ]
-            //                     }
-            //                 }
-            //             }
-            //         },
-            //         source: 'webhook-echo-sample'
-            //     });
-            // });
-        // })
-        // .catch(function (err) {
-        //     speech = "Il y a eu une erreur dans le process. Veuillez recommencer la saisie."
-        //     console.log(err);
-        // });
 });
 
 restService.post('/music', function(req, res) {
