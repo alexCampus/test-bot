873621c7115ea8204d22d943333c0c4c0e32c90f
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-06 11:37:28 +0100

    test for axios requete

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ restService.post('/map', function(req, resp) {
                                     image_url: $('.itemImage img', this).attr("src"),
                                     default_action: {
                                         type: "web_url",
-                                        url: url + $('h3 a', this).attr("href"),
+                                        url: 'www.fnaim.fr' + $('h3 a', this).attr("href"),
                                         webview_height_ratio: "tall"
                                     }
                                 };
