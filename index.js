15f13f57d0d95525ad5fed059e119304f4bdcc19
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-07 10:53:09 +0100

    test for axios requete

--- index.js
+++ index.js
@@ -16,15 +16,6 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
-    // var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: 'Hello my friend',
-        displayText: 'Hello my friend',
-        source: 'webhook-echo-sample'
-    });
-});
-
 restService.post('/map', function(req, resp) {
     let parameters = {};
     let url = configuration.fnaimUrlBuy;
@@ -34,7 +25,12 @@ restService.post('/map', function(req, resp) {
             console.log(res.data[0]);
             if (res.data[0].id == '') {
                 console.log('test if => ');
-                speech = "Désolé je n'ai pas compris votre recherche. Veuillez reformuler votre zone de recherche."
+                speech = "Désolé je n'ai pas compris votre recherche. Veuillez reformuler votre zone de recherche.";
+                return resp.json({
+                    speech: speech,
+                    displayText: speech,
+                    source: 'webhook-echo-sample'
+                });
             } else {
                 console.log('test else');
                 speech = res.data[0];
@@ -83,12 +79,6 @@ restService.post('/map', function(req, resp) {
                     parameters.PRIX_MAX = el.parameters.maxPrice;
                 }
             });
-            // if (req.body.result.contexts[0].parameters.GoodType[0] === 'maison') {
-            //     type = 2;
-            // } else if (req.body.result.contexts[0].parameters.GoodType[0] === 'appartement'){
-            //     type = 1;
-            // }
-
 
             console.log(parameters);
             axios.get(configuration.fnaimUrlBuy +
@@ -161,90 +151,6 @@ restService.post('/map', function(req, resp) {
             console.log(error);
         })
 });
-
-restService.post('/music', function(req, res) {
-    var speech = "";
-    switch (req.body.result.parameters.AudioSample.toLowerCase()) {
-        case "music":
-            speech = '<speak>  <audio src="https://actions.google.com/sounds/v1/ambiences/barnyard_with_animals.ogg">did not get your MP3 audio file</audio></speak>';
-            break;
-        case "delay":
-            speech = '<speak>Let me take a break for 3 seconds. <break time="3s"/> I am back again.</speak>';
-            break;
-    }
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
-    });
-});
-
-restService.post('/video', function(req, res) {
-    return res.json({
-        speech: '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-        displayText: '<speak>  <audio src="https://www.youtube.com/watch?v=VX7SSnvpj-8">did not get your MP3 audio file</audio></speak>',
-        source: 'webhook-echo-sample'
-    });
-});
-
-
-
-restService.post('/slack-test', function(req, res) {
-
-    var slack_message = {
-        "text": "Details of JIRA board for Browse and Commerce",
-        "attachments": [{
-            "title": "JIRA Board",
-            "title_link": "http://www.google.com",
-            "color": "#36a64f",
-
-            "fields": [{
-                "title": "Epic Count",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Story Count",
-                "value": "40",
-                "short": "false"
-            }],
-
-            "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-        }, {
-            "title": "Story status count",
-            "title_link": "http://www.google.com",
-            "color": "#f49e42",
-
-            "fields": [{
-                "title": "Not started",
-                "value": "50",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }, {
-                "title": "Development",
-                "value": "40",
-                "short": "false"
-            }]
-        }]
-    }
-    return res.json({
-        speech: "speech",
-        displayText: "speech",
-        source: 'webhook-echo-sample',
-        data: {
-            "facebook": slack_message
-        }
-    });
-});
-
-
-
 
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
