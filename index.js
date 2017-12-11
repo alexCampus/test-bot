608f9f6d70adf090dabf1694e746728cf0b2634b
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-11 10:11:48 +0100

    test get username facebook

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(bodyParser.json());
 
 restService.post('/map', function(req, resp) {
     // console.log(req.IncomingMessage);
-    console.log(req.body.originalRequest);
+    console.log(req.body.originalRequest.data.sender);
     // fonction.userInfoRequest(req.body.id);
     fonction.requeteFnaimCheckLocalisation(req, resp);
 });
