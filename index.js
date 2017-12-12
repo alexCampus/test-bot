5924fe501a9106e3c4f86a3d79c134b5ee2ae1d1
De Pembroke Alexandre <alexandre.depembroke-ext@businessdecision.com>
2017-12-12 09:16:14 +0100

    add eolas gmail compte

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ restService.use(bodyParser.json());
 
 restService.post('/map', function(req, resp) {
     console.log(req.body.result.action);
+    console.log(req.body.originalRequest);
     if (req.body.result.action === 'input.welcome') {
         fonction.userInfoRequest(resp, req.body.originalRequest.data.sender.id);
     } else {
